89bd3f6a48e9f0cb1d79f6cf5a01b6f5057f1920
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-29 16:17:18 -0400

    Added New Image Filters
    -Created a Sharpen Filter, Blur Filter,
    and a Mean Color Filter.

--- src/opencv-app/main.py
+++ src/opencv-app/main.py
@@ -12,7 +12,7 @@ image = cv2.imread(abs_file_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # print(image)
 
-# cv2.imshow('Original image', image)
+# cv2.imshow('Original Image', image)
 # cv2.waitKey(0)
 
 alpha = 1.2
@@ -20,5 +20,33 @@ beta = 50
 
 adjusted = cv2.convertScaleAbs(image, alpha = alpha, beta = beta)
 
-cv2.imshow('Adjusted image', adjusted)
-cv2.waitKey(0)
+# cv2.imshow('Adjusted Image', adjusted)
+# cv2.waitKey(0)
+
+#-----CUSTOM-KERNELS-----
+
+#__Sharpening Filter__:
+sharpen_kernel = np.array([[-1,-1,-1],
+                           [-1, 9,-1],
+                           [-1,-1,-1]])
+
+sharpened = cv2.filter2D(image, -1, sharpen_kernel)
+
+# cv2.imshow('Sharpened Image', sharpened)
+# cv2.waitKey(0)
+
+#__Blur Filter__:
+blur_kernel = np.array([[1,1,1],
+                        [1,1,1],
+                        [1,1,1]])/9
+
+blurred = cv2.filter2D(image, -1, blur_kernel)
+# cv2.imshow('Blurred Image', blurred)
+# cv2.waitKey(0)
+
+mean_values = np.mean(image, axis = (0, 1))
+
+print(f"Mean values for R, G, B: {mean_values}")
+
+mean_color_image = np.ones_like(image) * mean_values.astype(np.uint8)
+cv2.imshow('Mean Color Image', mean_color_image)
